2f4ada9878976d6ccdd39b2ea1eab8acc89d77b7
oderao <tripleo4u@gmail.com>
2022-06-02 17:58:11 +0100

    use bootstrap forms

--- invoiceapp/invoice/views.py
+++ invoiceapp/invoice/views.py
@@ -3,13 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from .models import Invoice
 
 
 def index(request):
+    model = Invoice
+    fields = ['description']
     template = loader.get_template('invoice/create_invoice.html')
     context = {
         'name_':'odera'
     }
     return HttpResponse(template.render(context, request))
 
-   
+    return HttpResponse("Hello, world. You're at the invoice index.")
